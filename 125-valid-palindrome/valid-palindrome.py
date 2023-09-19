64634a9fc18f07bb5c3309599778ce6a002ac398
Sushant Gangwani <81021277+sushant2812@users.noreply.github.com>
2023-09-18 21:49:42 -0700

    Time: 50 ms (62.76%) | Memory: 17.7 MB (35.15%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,7 +1,6 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         a=''.join(x for x in s if x.isalnum())
-        print(s)
         b=a.lower()
         c=list(b)
         c.reverse()
