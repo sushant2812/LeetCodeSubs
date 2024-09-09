0fc063206b0d17df5f29e6de6477da53b0f7fbbd
Sushant Gangwani <81021277+sushant2812@users.noreply.github.com>
2024-09-09 11:44:33 -0400

    Time: 93 ms (42.99%) | Memory: 20.3 MB (52.37%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,12 +1,11 @@
-import collections
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        a = {}
+        ans = {}
         for i in strs:
-            temp = list(collections.Counter(i).elements())
+            temp = list(i)
             temp.sort()
             temp = ''.join(temp)
-            if temp not in a:
-                a[temp] = []
-            a[temp].append(i)
-        return a.values()
+            if temp not in ans:
+                ans[temp] = []
+            ans[temp].append(i)
+        return ans.values()
