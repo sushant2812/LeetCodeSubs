f4639724f1410c9aa210231f3b6288fe9a831abf
Sushant Gangwani <81021277+sushant2812@users.noreply.github.com>
2024-09-11 17:32:54 -0400

    Time: 643 ms (79.34%) | Memory: 20.5 MB (86.25%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -3,14 +3,23 @@ class Solution:
         nums.sort()
         res = set()
         for index, num in enumerate(nums):
+            if index>0 and nums[index]==nums[index-1]: ##Skippin duplicate of num1
+                continue
             left = index+1
             right = len(nums)-1
             while left < right:
                 result = num+nums[left]+nums[right]
-                if result==0:
-                    res.add((num, nums[left],nums[right]))
                 if result>0:
                     right-=1
-                else:
+                elif result<0:
                     left+=1
+                else:
+                    res.add((num, nums[left],nums[right]))
+                    left=left+1
+                    right=right-1
+                    while left<right and nums[left]==nums[left-1]: ##Skippin duplicate of num2
+                        left+=1
+                    while left<right and nums[right]==nums[right+1]: ##Skippin duplicate of num3
+                        right-=1
+
         return res
