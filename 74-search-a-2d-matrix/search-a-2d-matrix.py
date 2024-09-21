98039e7731907401cb79f0ba0b0a24d9c6f1fde4
Sushant Gangwani <81021277+sushant2812@users.noreply.github.com>
2024-09-21 19:02:52 -0400

    Time: 47 ms (46.99%) | Memory: 17 MB (64.18%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/search-a-2d-matrix.py
@@ -1,28 +1,27 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        rows=len(matrix)
-        cols=len(matrix[0])
-        top = 0
-        bottom = rows - 1
-        while top <= bottom:
-            row = (top + bottom)//2
-            if target > matrix[row][-1]:
-                top = row + 1
-            elif target < matrix [row][0]:
-                bottom = row -1
+        top=0
+        bottom=len(matrix)-1
+        while top<=bottom:
+            row = (top+bottom)//2
+            if target>matrix[row][-1]: ## Bigger than the last element of the row
+                top = row+1
+            elif target<matrix[row][0]:
+                bottom=row-1
             else:
-                break
+                break ##Right Row!
         if not(top<=bottom):
-            return False
+            return False ##Nothin
         row = (top+bottom)//2
-        l = 0
-        r = cols -1
-        while l<=r:
-            num=(l+r)//2
-            if matrix[row][num]==target:
+        left = 0
+        print(row)
+        right = len(matrix[0])-1
+        while left<=right:
+            mid = (left+right)//2
+            if matrix[row][mid]==target:
                 return True
-            elif matrix[row][num]>target:
-                r=num-1
-            elif matrix[row][num]<target:
-                l=num+1
+            if matrix[row][mid]>target:
+                right=mid-1
+            else:
+                left=mid+1
         return False
