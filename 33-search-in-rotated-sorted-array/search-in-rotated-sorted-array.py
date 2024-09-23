8c45188ca49d82e0eb5f444383d6807c24cc8a4d
Sushant Gangwani <81021277+sushant2812@users.noreply.github.com>
2024-09-23 15:26:57 -0400

    Time: 42 ms (67.17%) | Memory: 17 MB (5.64%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -1,5 +1,26 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
+        left = 0 
+        right = len(nums)-1
+        while left<=right:
+            mid = (left+right)//2
+            if nums[mid]==target:  
+                return mid
+            if nums[mid]>=nums[left]: ##Left Half is Sorted if true
+                if nums[left]<=target<=nums[mid]: ##Target lies between center and left
+                    right = mid -1
+                else:
+                    left = mid+1
+            else:
+                if nums[mid]<= target <=nums[right]: ## Target lies between center and right therefore need to increment the left
+                    left= mid+1
+                else:
+                    right = mid -1
+        return -1
+
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
         left = 0
         right = len(nums)-1
         while left <= right:
