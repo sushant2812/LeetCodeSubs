1cc7f5cac3fe9767add7def380c196e0f6a7f458
Sushant Gangwani <81021277+sushant2812@users.noreply.github.com>
2024-09-23 15:59:35 -0400

    Time: 49 ms (90.23%) | Memory: 16.7 MB (59.99%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -15,7 +15,6 @@ class Solution:
             return True
         window_size = len(s1)
         for i in range(window_size,len(s2)):
-            print(s1_counter, s2_counter)
             s2_counter[ord(s2[i]) - 97] += 1
             s2_counter[ord(s2[i - window_size]) - 97] -= 1
             if s1_counter==s2_counter:
