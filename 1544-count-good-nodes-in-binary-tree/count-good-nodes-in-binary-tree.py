50dd7cf3fe6364cea7700f287b45dc25ad958f91
Sushant Gangwani <81021277+sushant2812@users.noreply.github.com>
2024-10-03 06:56:42 -0700

    Time: 135 ms (49.77%) | Memory: 31.6 MB (8.93%) - LeetSync

--- 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.py
+++ 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.py
@@ -5,15 +5,16 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def helper(self,root,val_to_compare):
-        if(root==None):
-            return 0
-        if(root.val>=val_to_compare):
-            self.cnt[0]=self.cnt[0]+1
-        self.helper(root.left,max(val_to_compare,root.val))
-        self.helper(root.right,max(val_to_compare,root.val))
-
     def goodNodes(self, root: TreeNode) -> int:
-        self.cnt = [0]
-        self.helper(root,float('-inf'))
-        return self.cnt[0]
+        def dfs(node,currMax,cnt):
+            if (node==None):
+                return 0
+            if(node.val>=currMax):
+                cnt = 1
+            else:
+                cnt = 0
+            currMax = max(currMax,node.val)
+            cnt += dfs(node.left,currMax,cnt)
+            cnt += dfs(node.right,currMax,cnt)
+            return cnt
+        return dfs(root,root.val,0)
