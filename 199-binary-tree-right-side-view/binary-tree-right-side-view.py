c6f38916806552c75cb6859efad00e8d0cfeec64
Sushant Gangwani <81021277+sushant2812@users.noreply.github.com>
2024-10-02 17:37:46 -0700

    Time: 38 ms (44.75%) | Memory: 16.7 MB (5.53%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.py
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.py
@@ -5,15 +5,20 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def helper(self,root,level):
-        if root == None:
-            return
-        if level==len(self.ans):
-            self.ans.append(root.val)
-        self.helper(root.right,level+1)
-        self.helper(root.left,level+1)
-        
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
-        self.ans=[]
-        self.helper(root,0)
-        return self.ans
+        res = []
+        if root is None:
+            return []
+        queue = [root]
+        node = root
+        while queue!=[]:
+            length = len(queue)
+            for i in range(length):
+                node = queue.pop(0)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            res.append(node.val)
+        print(res)
+        return res
