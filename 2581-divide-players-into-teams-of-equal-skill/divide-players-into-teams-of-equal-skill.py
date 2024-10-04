763c2135673c27373c0f8765c78d184cf2dd126d
Sushant Gangwani <81021277+sushant2812@users.noreply.github.com>
2024-10-03 17:14:09 -0700

    Time: 386 ms (89.03%) | Memory: 29.6 MB (31.15%) - LeetSync

--- 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.py
+++ 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.py
@@ -1,16 +1,21 @@
 class Solution:
     def dividePlayers(self, skill: List[int]) -> int:
-        skill.sort()
-        print(skill)      
-        strong = skill.pop(len(skill)-1)
-        weak = skill.pop(0)
+        skill.sort()   
+        left = 0
+        right = len(skill)-1
+        strong = skill[right]
+        weak = skill[left]
         match = weak+strong
         res = weak*strong
-        while skill!=[]:
-            strong = skill.pop(len(skill)-1)
-            weak = skill.pop(0)
+        left = left+1
+        right = right-1
+        while left<right:
+            strong = skill[right]
+            weak = skill[left]
             if strong+weak!=match:
                 return -1
             else:
                 res += strong*weak
+            left=left+1
+            right=right-1
         return res
