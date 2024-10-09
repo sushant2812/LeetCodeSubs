43eabb03219799447d97e3bde22eb6de6b27d8f4
Sushant Gangwani <81021277+sushant2812@users.noreply.github.com>
2024-10-09 15:39:02 -0700

    Time: 37 ms (77.72%) | Memory: 16.8 MB (10.24%) - LeetSync

--- 46-permutations/permutations.py
+++ 46-permutations/permutations.py
@@ -3,16 +3,15 @@ class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         res = []
         counter = Counter(nums)
-        def backtrack(subArray):
-            if len(nums)==len(subArray):
-                res.append(subArray.copy())
+        def dfs(temp):
+            if len(temp)==len(nums):
+                res.append(temp.copy())
                 return
             for num in nums:
                 if counter[num]==0:
                     continue
-                counter[num]-=1
-                backtrack(subArray + [num])
-                counter[num]+=1
-                
-        backtrack([])
+                counter[num] -= 1
+                dfs(temp+[num])
+                counter[num] += 1
+        dfs([])
         return res
