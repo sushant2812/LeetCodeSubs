7a09cd0af79b358ca1b7875e7d2f61bc93ae4a48
Sushant Gangwani <81021277+sushant2812@users.noreply.github.com>
2024-10-09 14:51:18 -0700

    Time: 51 ms (70.43%) | Memory: 17 MB (32.36%) - LeetSync

--- 47-permutations-ii/permutations-ii.py
+++ 47-permutations-ii/permutations-ii.py
@@ -2,20 +2,17 @@ from collections import Counter
 class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
         res = []
-        hashmap = [0] * len(nums)
-        def backtrack(subArray,hashmap):
+        counter = Counter(nums)
+        def backtrack(subArray):
             if len(nums)==len(subArray):
-                
-                if subArray.copy() not in res:
-                    res.append(subArray.copy())
-                    return
-            for i in range(len(nums)):
-                if hashmap[i]==1:
+                res.append(subArray.copy())
+                return
+            for key in counter.keys():
+                if counter[key]==0:
                     continue
-                hashmap[i] = 1
-                subArray.append(nums[i])
-                backtrack(subArray,hashmap)
-                subArray.pop()
-                hashmap[i] = 0
-        backtrack([],hashmap)
+                counter[key]-=1
+                backtrack(subArray + [key])
+                counter[key]+=1
+                
+        backtrack([])
         return res
