6c5fe8a2f983cfb2b6a8b86ade024369f1e91274
Sushant Gangwani <81021277+sushant2812@users.noreply.github.com>
2024-10-08 18:03:15 -0700

    Time: 37 ms (43.66%) | Memory: 16.6 MB (15.75%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -3,13 +3,16 @@ class Solution:
         if not digits:
             return []
         output = []
+        combination = []
         phone_map = {'2':'abc','3':'def','4':'ghi','5':'jkl','6':'mno','7':'pqrs','8':'tuv','9':'wxyz'}
-        def backtrack(combination,next_digits):
-            if len(next_digits)==0:
-                output.append(combination)
+        def backtrack(idx):
+            if idx==len(digits):
+                output.append(''.join(combination))
                 return
             else:
-                for letter in phone_map[next_digits[0]]:
-                    backtrack(combination+letter,next_digits[1:])
-        backtrack('',digits)
+                for letter in phone_map[digits[idx]]:
+                    combination.append(letter)
+                    backtrack(idx+1)
+                    combination.pop()
+        backtrack(0)
         return output
