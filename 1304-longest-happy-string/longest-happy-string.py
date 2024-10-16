1bb5cb22320854238884f959b12d8772a031b721
Sushant Gangwani <81021277+sushant2812@users.noreply.github.com>
2024-10-16 15:27:25 -0700

    Time: 42 ms (12.69%) | Memory: 16.5 MB (68.55%) - LeetSync

--- 1304-longest-happy-string/longest-happy-string.py
+++ 1304-longest-happy-string/longest-happy-string.py
@@ -11,16 +11,16 @@ class Solution:
             heapq.heappush(heap,(-c,'c'))
         while heap:
             count1, char1 = heapq.heappop(heap)
-            if len(result) >= 2 and result[-1] == result[-2] == char1:
+            if len(result)>=2 and result[-1]==result[-2]==char1:
                 if not heap:
                     break
                 count2, char2 = heapq.heappop(heap)
-                result += char2
+                result+=char2
                 if count2+1<0:
                     heapq.heappush(heap,(count2+1,char2))
                 heapq.heappush(heap,(count1,char1))
             else:
-                result += char1
+                result+=char1
                 if count1+1<0:
                     heapq.heappush(heap,(count1+1,char1))
         return result
