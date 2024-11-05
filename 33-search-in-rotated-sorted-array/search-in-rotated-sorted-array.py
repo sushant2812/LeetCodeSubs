a73326f988e2fb0fa11c29cbeb3a3030cb97d9ea
Sushant Gangwani <81021277+sushant2812@users.noreply.github.com>
2024-11-05 12:27:50 -0500

    Time: 0 ms (100.00%) | Memory: 17 MB (14.76%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -1,40 +1,19 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        left = 0 
-        right = len(nums)-1
-        while left<=right:
-            mid = (left+right)//2
-            if nums[mid]==target:  
-                return mid
-            if nums[mid]>=nums[left]: ##Left Half is Sorted if true
-                if nums[left]<=target<=nums[mid]: ##Target lies between center and left
-                    right = mid -1
-                else:
-                    left = mid+1
-            else:
-                if nums[mid]<= target <=nums[right]: ## Target lies between center and right therefore need to increment the left
-                    left= mid+1
-                else:
-                    right = mid -1
-        return -1
-
-
-class Solution:
-    def search(self, nums: List[int], target: int) -> int:
         left = 0
-        right = len(nums)-1
+        right = len(nums) - 1
         while left <= right:
-            mid = (left+right)//2
+            mid = (left + right) // 2
             if nums[mid] == target:
                 return mid
-            if nums[mid]>=nums[left]: ##All numbers are sorted between left and mid
-                if nums[left]<=target<=nums[mid]: ##IF true then the target is between these two numbers
-                    right = mid-1
+            if nums[left] <= nums[mid]:  # Left side is sorted
+                if nums[left] <= target <= nums[mid]:
+                    right = mid - 1  # Search in the left half
                 else:
-                    left = mid+1
-            else:
-                if nums[mid]<=target<=nums[right]:
-                    left = mid+1
+                    left = mid + 1  # Search in the right half
+            else:  # Right side is sorted
+                if nums[mid] <= target <= nums[right]:
+                    left = mid + 1  # Search in the right half
                 else:
-                    right = mid-1
+                    right = mid - 1  # Search in the left half
         return -1
