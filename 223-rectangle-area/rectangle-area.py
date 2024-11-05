e2e84332852602132d38a31a4872e6b116aee37d
Sushant Gangwani <81021277+sushant2812@users.noreply.github.com>
2024-11-05 13:05:46 -0500

    Time: 7 ms (13.54%) | Memory: 16.8 MB (27.88%) - LeetSync

--- 223-rectangle-area/rectangle-area.py
+++ 223-rectangle-area/rectangle-area.py
@@ -1,9 +1,9 @@
 class Solution:
     def computeArea(self, ax1: int, ay1: int, ax2: int, ay2: int, bx1: int, by1: int, bx2: int, by2: int) -> int:
-        cx1 = max(ax1, bx1)
-        cx2 = min(ax2, bx2)
-        cy1 = max(ay1, by1)
-        cy2 = min(ay2, by2)
+        cx1 = max(ax1, bx1) #MAX OF LOWER
+        cx2 = min(ax2, bx2) #MIN OF UPPER
+        cy1 = max(ay1, by1) #MAX OF LOWER
+        cy2 = min(ay2, by2) #MIN OF UPPER
         area =max(0,cx2-cx1)*max(0,cy2-cy1)
         area_first = abs(ax1-ax2)*abs(ay1-ay2)
         area_second = abs(bx1-bx2)*abs(by1-by2)
