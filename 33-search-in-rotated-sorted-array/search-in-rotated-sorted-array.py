82fc67bf16388fc257d2a252d382075fedf83be9
Sushant Gangwani <81021277+sushant2812@users.noreply.github.com>
2024-11-05 12:33:43 -0500

    Time: 0 ms (100.00%) | Memory: 16.9 MB (83.66%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -6,14 +6,30 @@ class Solution:
             mid = (left + right) // 2
             if nums[mid] == target:
                 return mid
-            if nums[left] <= nums[mid]:  # Left side is sorted
-                if nums[left] <= target <= nums[mid]:
-                    right = mid - 1  # Search in the left half
+            if nums[left] <= nums[mid]: #LEFT SIDE SORTED
+                if nums[left]<=target<=nums[mid]:
+                    right=mid-1
                 else:
-                    left = mid + 1  # Search in the right half
-            else:  # Right side is sorted
-                if nums[mid] <= target <= nums[right]:
-                    left = mid + 1  # Search in the right half
+                    left=mid+1
+            else:
+                if nums[mid]<=target<=nums[right]:
+                    left=mid+1
                 else:
-                    right = mid - 1  # Search in the left half
+                    right=mid-1
         return -1
+
+
+
+
+
+
+            #         if nums[left] <= nums[mid]:  # Left side is sorted
+            #     if nums[left] <= target <= nums[mid]:
+            #         right = mid - 1  # Search in the left half
+            #     else:
+            #         left = mid + 1  # Search in the right half
+            # else:  # Right side is sorted
+            #     if nums[mid] <= target <= nums[right]:
+            #         left = mid + 1  # Search in the right half
+            #     else:
+            #         right = mid - 1  # Search in the left half
