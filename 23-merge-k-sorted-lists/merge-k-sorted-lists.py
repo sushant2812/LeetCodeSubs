bb36538a4d35b562283738c3b093d13d22ca21d8
Sushant Gangwani <81021277+sushant2812@users.noreply.github.com>
2024-11-07 16:21:20 -0500

    Time: 3 ms (99.28%) | Memory: 19.8 MB (37.30%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.py
@@ -4,23 +4,23 @@
 #         self.val = val
 #         self.next = next
 from heapq import heappush, heappop
-def heapsort(iterable):
+def heapsort(lst):
     h = []
-    for value in iterable:
-        heappush(h, value)
-    return [heappop(h) for i in range(len(h))]
-
+    for i in range(len(lst)):
+        heappush(h,lst[i])
+    return [heappop(h) for i in range(len(lst))]
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
         values = []
-        for a in lists:
-            while a:
-                values.append(a.val)
-                a=a.next
-        values = heapsort(values)
-        head = ListNode()
-        temp = head
-        for num in values:
+        for lst in lists:
+            while lst:
+                values.append(lst.val)
+                lst=lst.next
+        
+        new_values = heapsort(values)
+        head=ListNode()
+        temp=head 
+        for num in new_values:
             temp.next = ListNode(num)
             temp = temp.next
         return head.next
