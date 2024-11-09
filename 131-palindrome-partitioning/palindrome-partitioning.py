45b41f2ccd1a617daee78aea0fb8f927dcb16fc9
Sushant Gangwani <81021277+sushant2812@users.noreply.github.com>
2024-11-09 17:16:52 -0500

    Time: 55 ms (32.66%) | Memory: 33.9 MB (64.49%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.py
+++ 131-palindrome-partitioning/palindrome-partitioning.py
@@ -2,17 +2,17 @@ class Solution:
     def partition(self, s: str) -> List[List[str]]:
         def isPalindrome(l,r):
             substr = s[l:r+1]
-            return substr == substr[::-1]
-        res = []
-        path = []
+            return substr==substr[::-1]
+        res=[]
+        temp=[]
         def dfs(idx):
             if idx>=len(s):
-                res.append(path.copy())
+                res.append(temp.copy())
                 return
             for right in range(idx,len(s)):
                 if isPalindrome(idx,right):
-                    path.append(s[idx:right+1])
+                    temp.append(s[idx:right+1])
                     dfs(right+1)
-                    path.pop()
+                    temp.pop()
         dfs(0)
         return res
