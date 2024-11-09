df6dbe3e75c09e100bfafc68c6a8878242c91005
Sushant Gangwani <81021277+sushant2812@users.noreply.github.com>
2024-11-09 17:22:25 -0500

    Time: 0 ms (100.00%) | Memory: 16.9 MB (5.68%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -1,17 +1,17 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        if not digits:
+        if digits=="":
             return []
-        output = []
-        combination = []
-        phone_map = {'2':'abc','3':'def','4':'ghi','5':'jkl','6':'mno','7':'pqrs','8':'tuv','9':'wxyz'}
-        def backtrack(idx):
+        num_combo={'2':'abc','3':'def','4':'ghi','5':'jkl','6':'mno','7':'pqrs','8':'tuv','9':'wxyz'}
+        res=[]
+        temp=[]
+        def dfs(idx):
             if idx==len(digits):
-                output.append(''.join(combination))
+                res.append(''.join(temp))
                 return
-            for char in phone_map[digits[idx]]:
-                combination.append(char)
-                backtrack(idx+1)
-                combination.pop()
-        backtrack(0)
-        return output
+            for char in num_combo[digits[idx]]:
+                temp.append(char)
+                dfs(idx+1)
+                temp.pop()
+        dfs(0)
+        return res
