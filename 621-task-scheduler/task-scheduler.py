0c9db10d8f2a79e7a14a27b53afd4e5fb274b022
Sushant Gangwani <81021277+sushant2812@users.noreply.github.com>
2024-11-10 18:21:29 -0500

    Time: 153 ms (24.78%) | Memory: 18.7 MB (8.42%) - LeetSync

--- 621-task-scheduler/task-scheduler.py
+++ 621-task-scheduler/task-scheduler.py
@@ -2,20 +2,18 @@ class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
         freq = Counter(tasks)
         heap = []
-        cooldown = deque()
+        cooldown = [] ## (task_count,time), time= the next timer the task is available to run
         timer = 0
         for key,value in freq.items():
             heapq.heappush(heap,-value)
         
         while heap or cooldown:
             if heap:
-                task = -heapq.heappop(heap)
+                task= -heapq.heappop(heap)
                 if task>1:
                     cooldown.append((task-1,timer+n+1))
             timer+=1
-
             while cooldown and cooldown[0][1]==timer:
-                task_count, next_iteration = cooldown[0]
-                cooldown.popleft()
+                task_count, next_available = cooldown.pop(0)
                 heapq.heappush(heap,-task_count)
         return timer
