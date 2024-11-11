725b53c23445b480c2380bdd45ea6c924a9d1e9e
Sushant Gangwani <81021277+sushant2812@users.noreply.github.com>
2024-11-11 14:51:22 -0500

    Time: 242 ms (56.53%) | Memory: 28.1 MB (5.26%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -4,7 +4,7 @@ class Solution:
         len_col = len(grid[0])
         res = 0
         visited =set()
-        def dfs(i,j,visited):
+        def bfs(i,j):
             if i<0 or i>=len_row or j<0 or j>=len_col:
                 return
             if (i,j) in visited or grid[i][j]!="1":
@@ -12,13 +12,13 @@ class Solution:
 
             grid[i][j]="0"
             visited.add((i,j))
-            dfs(i+1,j,visited)
-            dfs(i-1,j,visited)
-            dfs(i,j-1,visited)
-            dfs(i,j+1,visited)
+            bfs(i+1,j)
+            bfs(i-1,j)
+            bfs(i,j-1)
+            bfs(i,j+1)
         for i in range(len_row):
             for j in range(len_col):
                 if grid[i][j]=="1":
                     res+=1
-                    dfs(i,j,visited)
+                    bfs(i,j)
         return res
