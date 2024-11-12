0e999bc3dc472106b26796fd40cbcc349e0f9f33
Sushant Gangwani <81021277+sushant2812@users.noreply.github.com>
2024-11-11 20:49:33 -0500

    Time: 258 ms (36.99%) | Memory: 28 MB (5.26%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,24 +1,22 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        len_row = len(grid)
-        len_col = len(grid[0])
-        res = 0
+        rows=len(grid)
+        cols=len(grid[0])
+        res=0
         visited =set()
-        def bfs(i,j):
-            if i<0 or i>=len_row or j<0 or j>=len_col:
+        def dfs(i,j):
+            if (i<0 or i>=rows) or (j<0 or j>=cols) or grid[i][j]=="0" or grid[i][j] in visited:
                 return
-            if (i,j) in visited or grid[i][j]!="1":
-                return
-
             grid[i][j]="0"
             visited.add((i,j))
-            bfs(i+1,j)
-            bfs(i-1,j)
-            bfs(i,j-1)
-            bfs(i,j+1)
-        for i in range(len_row):
-            for j in range(len_col):
-                if grid[i][j]=="1":
+            dfs(i+1,j)
+            dfs(i-1,j)
+            dfs(i,j-1)
+            dfs(i,j+1)
+        
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c]=='1':
                     res+=1
-                    bfs(i,j)
+                    dfs(r,c)
         return res
