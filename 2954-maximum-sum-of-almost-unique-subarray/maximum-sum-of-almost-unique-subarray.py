45c724db6f50ef18bc7561d7c3496f38079b63ad
Sushant Gangwani <81021277+sushant2812@users.noreply.github.com>
2024-11-19 13:35:19 -0500

    Time: 82 ms (88.44%) | Memory: 19.9 MB (86.75%) - LeetSync

---
+++ 2954-maximum-sum-of-almost-unique-subarray/maximum-sum-of-almost-unique-subarray.py
@@ -0,0 +1,32 @@
+class Solution:
+    def maxSum(self, nums: List[int], m: int, k: int) -> int:
+        ans=0
+        freq={}
+        subarr=deque()
+        invalid=0
+        temp_sum=0
+        for i in range(k):
+            subarr.append(nums[i])
+            temp_sum+=nums[i]
+            if nums[i] not in freq:
+                freq[nums[i]]=1
+            else:
+                freq[nums[i]]+=1
+        if len(freq)==k or len(freq)>=m:
+            ans=max(ans,temp_sum)
+
+        for i in range(k,len(nums)):
+            a=subarr.popleft()
+            temp_sum-=a
+            freq[a]-=1
+            if freq[a]==0:
+                del freq[a]
+            subarr.append(nums[i])
+            temp_sum+=nums[i]
+            if nums[i] not in freq:
+                freq[nums[i]]=1
+            else:
+                freq[nums[i]]+=1
+            if len(freq)==k or len(freq)>=m:
+                ans=max(ans,temp_sum)
+        return ans
