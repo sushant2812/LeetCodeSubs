9b8a4c8807a7f369fc0b817788f5143489ac4233
Sushant Gangwani <81021277+sushant2812@users.noreply.github.com>
2024-11-20 14:15:27 -0500

    Time: 362 ms (28.81%) | Memory: 17.5 MB (98.42%) - LeetSync

--- 127-word-ladder/word-ladder.py
+++ 127-word-ladder/word-ladder.py
@@ -1,22 +1,21 @@
-from collections import deque
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
-        l={}
-        for i in wordList:
-            l[i]=1
-        q=deque()
-        q.append((beginWord,1))
+        if (endWord not in wordList) or (beginWord == endWord):
+            return 0
+        words, res = set(wordList), 0
+        q = deque([beginWord])
         while q:
-            word=q[0][0]
-            steps=q[0][1]
-            if word in l:
-                del l[word]
-            q.popleft()
-            for i in range(len(word)):
-                for char in string.ascii_lowercase:
-                    zword=word[:i]+char+word[i+1:]
-                    if zword in l:
-                        q.append((zword,steps+1))
-                    if zword==endWord and zword in l:
-                        return steps+1
+            res += 1
+            for _ in range(len(q)):
+                node = q.popleft()
+                if node == endWord:
+                    return res
+                for i in range(len(node)):
+                    for c in range(97, 123):
+                        if chr(c) == node[i]:
+                            continue
+                        nei = node[:i] + chr(c) + node[i + 1:]
+                        if nei in words:
+                            q.append(nei)
+                            words.remove(nei)
         return 0
