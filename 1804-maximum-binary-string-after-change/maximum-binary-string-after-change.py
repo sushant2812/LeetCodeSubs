894c0afa3ebef7de2388b956cf61a91ef1b0320a
Sushant Gangwani <81021277+sushant2812@users.noreply.github.com>
2024-11-21 13:27:59 -0500

    Time: 15 ms (75.86%) | Memory: 17.6 MB (98.37%) - LeetSync

--- 1804-maximum-binary-string-after-change/maximum-binary-string-after-change.py
+++ 1804-maximum-binary-string-after-change/maximum-binary-string-after-change.py
@@ -1,15 +1,8 @@
 class Solution:
     def maximumBinaryString(self, binary: str) -> str:
-        arr=[]
-        stack=[]
-        for i in binary:
-            stack.append(i)
-            
-            if stack[0] == '1':
-                arr.append('1')
-                stack.pop()
-            elif stack[0] == '0' and stack[-1] == '0' and len(stack) > 1:
-                arr.append('1')
-                stack.pop()
 
-        return ''.join(arr+stack)
+        if not '0' in binary: return binary
+        left0, zeros = binary.find('0'), binary.count('0')
+        
+        ans = '1'*(left0+zeros-1) + '0'
+        return ans.ljust(len(binary), '1')
