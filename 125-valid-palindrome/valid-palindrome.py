e4049a9b8a712e0c745fd4e16e29a16ad892685b
Sushant Gangwani <81021277+sushant2812@users.noreply.github.com>
2024-12-08 16:54:17 -0500

    Time: 11 ms (37.12%) | Memory: 18.1 MB (26.22%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -3,7 +3,7 @@ class Solution:
         final_string = ''.join(x for x in s if x.isalnum())
         final_string = final_string.lower()
         left = 0
-        right = len(final_string)-1
+        right  = len(final_string)-1
         while left<=right:
             if(final_string[left]==final_string[right]):
                 left+=1
