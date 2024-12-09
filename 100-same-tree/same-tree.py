d2859bbafc3279b3cf11d364c243d2cd603f7e9c
Sushant Gangwani <81021277+sushant2812@users.noreply.github.com>
2024-12-08 20:34:06 -0500

    Time: 0 ms (100.00%) | Memory: 17.5 MB (7.00%) - LeetSync

--- 100-same-tree/same-tree.py
+++ 100-same-tree/same-tree.py
@@ -11,5 +11,5 @@ class Solution:
         if p==None or q==None:
             return False
         if p.val==q.val:
-            return self.isSameTree(p.right,q.right) and self.isSameTree(p.left,q.left)
+            return self.isSameTree(p.left,q.left) and self.isSameTree(p.right,q.right)
         return False
